d48a12ae52660b48673438c0b3e7bfdc05dae9bb
knu9910 <knu9910@naver.com>
2025-02-28 17:45:42 +0900

    Prisma Module 글로벌 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [UsersModule, PrismaModule], // ✅ UsersService를 여기서 제거해야 함
   controllers: [],
-  providers: [],
+  providers: [], // ✅ UsersService를 providers에 추가
 })
 export class AppModule {}
